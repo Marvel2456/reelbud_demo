d00bd044b4872826da093ca1739e4af4953bb578
Marvel2456 <marvellousidemudia@gmail.com>
2023-06-07 20:55:59 +0100

    Setup react and tailwind for the project

---
+++ reelbud/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container max-w-screen-xl mx-auto">
+      <div className=''>
+        <h1>nano cow</h1>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
